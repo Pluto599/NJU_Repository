489c693efe5457a5a8e8723fd1f8c85f89578089
ranshaobaichi <145732634+ranshaobaichi@users.noreply.github.com>
2024-11-30 20:30:49 +0800

    PA3-1

--- RSBC/PA3-1/cache.h
+++ RSBC/PA3-1/cache.h
@@ -1,8 +1,6 @@
 #ifndef __CACHE_H__
 #define __CACHE_H__
 
-#ifdef CACHE_ENABLED
-
 typedef struct{
     bool valid_bit;
     uint32_t tag;
@@ -17,6 +15,5 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data);
 
 // read data from cache
 uint32_t cache_read(paddr_t paddr, size_t len);
-#endif
 
 #endif
